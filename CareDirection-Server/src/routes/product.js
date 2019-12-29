e121185b5ed17948e326fded0b64fbe30970c24b
DaEunShim <10ekdms01@naver.com>
2019-12-30 00:25:05 +0900

    modify products.js filename to product.js

--- CareDirection-Server/src/routes/product.js
+++ CareDirection-Server/src/routes/product.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 
-const products = Router()
+const product = Router()
 
 const productCtrl = require('../controller/productController')
 const { multer } = require('../../config/multer')
@@ -9,16 +9,16 @@ const needAuth = require('../middlewares/userCheck')
 const upload = multer('product')
 
 // 복용제품 등록 위한 정보 가져오기
-products.get('/:product_idx/dose', needAuth, productCtrl.importDose)
+product.get('/:product_idx/dose', needAuth, productCtrl.importDose)
 // 현재 복용제품 추가
-products.post('/:product_idx/dose', needAuth, productCtrl.enrollDose)
+product.post('/:product_idx/dose', needAuth, productCtrl.enrollDose)
 // 현재 복용 제품 정보수정
 // products.put('/:product_idx/dose', needAuth, productsCtrl.dose)
 
 // ADMIN 제품 등록하기
-products.post('/', upload.single('file'), productCtrl.insertProduct)
+product.post('/', upload.single('file'), productCtrl.insertProduct)
 // 복용 제품 (오늘 복용), post
-products.post('/product/:product_idx/dose/check', needAuth, productCtrl.checkProductDose)
+product.post('/product/:product_idx/dose/check', needAuth, productCtrl.checkProductDose)
 
 /*
 // 제품 디테일
@@ -30,4 +30,4 @@ products.get('/:productIdx/efficacy', needAuth, productsCtrl)
 // 최저가 정보
 products.get('/:productIdx/lowprice', needAuth, productsCtrl)
 */
-module.exports = products
+module.exports = product

868b0c15ed18236cb949c366b3f13f5e1518dda3
seunghee63 <0603yang@naver.com>
2019-12-30 05:24:25 +0900

    Add jwt to signin api

--- CareDirection-Server/src/controller/userController.js
+++ CareDirection-Server/src/controller/userController.js
@@ -27,3 +27,33 @@ exports.signUp = async (req, res) => {
     response.respondOnError(e.message, res, statusCode.INTERNAL_SERVER_ERROR)
   }
 }
+
+exports.signIn = async (req, res) => {
+  const { user_id, user_pw } = req.body
+
+  const schema = Joi.object({
+    user_id: Joi.string().required(),
+    user_pw: Joi.string().required(),
+  })
+
+  const validationData = { user_id, user_pw }
+
+  try {
+    const { error } = await schema.validateAsync(validationData)
+
+    if (error) {
+      response.respondOnError(message.NULL_VALUE, res, statusCode.FORBIDDEN)
+    }
+
+    const result = await userService.signIn(validationData)
+    console.log('test0', result)
+
+    if (result != null) {
+      return response.respondJson(message.SIGN_IN_SUCCESS, { token: result }, res, statusCode.OK)
+    }
+    return response.respondJsonWithoutData(message.SIGN_IN_PASSWORD_ERROR, res, statusCode.FORBIDDEN)
+
+  } catch (e) {
+    return response.respondOnError(e.message, res, statusCode.INTERNAL_SERVER_ERROR)
+  }
+}

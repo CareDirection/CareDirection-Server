a29ad74171fbfff29c6cd8113d95ee08e4d593a6
2Re-play <wogusduddnjs@gmail.com>
2020-01-02 10:47:20 +0900

    close #35

--- CareDirection-Server/src/controller/articleController.js
+++ CareDirection-Server/src/controller/articleController.js
@@ -1,22 +1,22 @@
-const Joi = require('@hapi/joi')
 const articleService = require('../service/articleService')
 const response = require('../lib/response')
 const message = require('../lib/responseMessage')
 const statusCode = require('../lib/statusCode')
 
-exports.insertArticle = async (req, res) => {
-  const validationChecker = Joi.object({
-    efficacy_name: Joi.string().required(),
-  })
-
+exports.insertMainArticle = async (req, res, next) => {
   try {
-    const validationResult = await validationChecker.validateAsync(req.body)
-    if (validationResult.error) {
-      throw new Error(403)
-    }
+    await articleService.insertMainArticle(req, next)
+    response.respondJsonWithoutData(message.SUCCESS, res, statusCode.CREATED)
+  } catch (e) {
+    console.log(e.message)
+    response.respondOnError(message.INSERT_FAILED, res, statusCode.FORBIDDEN)
+  }
+}
 
-    await articleService.insertEfficacy(req)
-    response.respondJsonWithoutData(message.EFFICACY_INSERT_SUCCESS, res, statusCode.CREATED)
+exports.insertSubArticle = async (req, res, next) => {
+  try {
+    await articleService.insertSubArticle(req, next)
+    response.respondJsonWithoutData(message.SUCCESS, res, statusCode.CREATED)
   } catch (e) {
     console.log(e.message)
     response.respondOnError(message.INSERT_FAILED, res, statusCode.FORBIDDEN)
--- CareDirection-Server/src/routes/article.js
+++ CareDirection-Server/src/routes/article.js
@@ -9,7 +9,8 @@ const articleController = require('../controller/articleController')
 const upload_main = multer('article/main')
 const upload_sub = multer('article/sub')
 
-artcle.post('/', upload_main.single('main_file'), upload_sub.fields([{ name: 'sub_file', maxCount: 8 }]), articleController.insertArticle)
-
+artcle.post('/main', upload_main.single('main_file'), articleController.insertMainArticle)
+// upload_sub.fields([{ name: 'sub_file', maxCount: 8 }]), insertSubArticle
+artcle.post('/:article_idx/sub', upload_sub.single('sub_file'), articleController.insertSubArticle)
 
 module.exports = artcle

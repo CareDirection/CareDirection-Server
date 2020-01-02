8dbbd61500d52f0cec3d6035dd3882a02bdb0383
Malibin <nightmare73@naver.com>
2020-01-02 19:01:03 +0900

    [add] 제품 기준 필터 쿼리 작성 완료

---
+++ CareDirection-Server/src/dao/standardDao.js
@@ -0,0 +1,15 @@
+exports.getEfficacyList = (connection, productIdx) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+        SELECT p.product_standard1, p.product_standard2, p.product_standard3, fc.filter_category_first_first, fc.filter_category_first_second, fc.filter_category_first_third, fc.filter_category_first_fourth, fc.filter_category_first_unit, fc.filter_category_second_first,fc.filter_category_second_second, fc.filter_category_third_first, fc.filter_category_third_second
+        FROM filter_category fc
+            JOIN product p 
+            ON fc.filter_category_idx = p.filter_category_idx
+        WHERE p.product_idx = ${productIdx}
+    `
+    connection.query(query, (err, result) => {
+      err && reject(err)
+      resolve(result[0])
+    })
+  })
+}

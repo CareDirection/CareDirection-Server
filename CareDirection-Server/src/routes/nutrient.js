b25837f14ab94a90e11c14014f372440f1f8059d
2Re-play <wogusduddnjs@gmail.com>
2020-01-02 19:08:09 +0900

    라우터 분기 처리 수정

--- CareDirection-Server/src/routes/nutrient.js
+++ CareDirection-Server/src/routes/nutrient.js
@@ -11,6 +11,7 @@ const upload = multer('ect')
 
 nutrient.post('/', upload.single('image'), nutrientController.insertNutrient)
 nutrient.get('/function', needAuth, nutrientController.getMyFunctioinalNutrients)
+
 nutrient.get('/:nutrient_idx', needAuth, nutrientController.specificInfo)
 
 module.exports = nutrient
